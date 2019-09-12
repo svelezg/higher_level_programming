1f9e33c172ed2e4335d847851721a334b3142edf
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-12 18:47:00 +0000

    task 102

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import dis
+
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+    else:
+        return sub(a, b)
+
+    for i in range(a, b):
+        c = add(c, i)
+    return c
+
+dis.dis(magic_calculation)
