d0eb0679d3e0e9837951b81ce5fe4445a5285e58
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-12 16:54:45 +0000

    task 0 to 5

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{:d} {}".format(len(sys.argv) - 1, "arguments."))
+    else:
+        if len(sys.argv) == 2:
+            print("{:d} {}".format(len(sys.argv) - 1, "argument:"))
+            print("{} {}".format(1, sys.argv[1]))
+        else:
+            print("{:d}: {}".format(len(sys.argv) - 1, "arguments:"))
+            for i in range(len(sys.argv))[1:]:
+                print("{:d}: {}".format(i - 1, sys.argv[i]))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    import sys
+
+    a = dir(hidden_4)
+    for i in range(len(a)):
+        if a[i][:2] != "__":
+            print("{}".format(a[i]))
