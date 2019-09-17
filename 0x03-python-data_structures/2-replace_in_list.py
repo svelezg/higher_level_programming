00d84603da52fcd820cdbb7be9dc4207cb810694
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-17 18:02:57 +0000

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return 
+        return
     if idx > len(my_list) - 1:
         return
     my_list[idx] = element
