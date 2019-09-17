d02f707a200e30ff2f524199b6ad24359287f3e7
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-17 20:16:38 +0000

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_python_list_info- Prints some basic info about Python lists.
+ * @p: PyObject
+ */
+
+void print_python_list_info(PyObject *p)
+{
+
+}
