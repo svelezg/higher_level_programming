eba7a50ee24551a2931f28baf1c34e0444afa69d
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-19 20:27:13 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) is not str:
+        return None
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     number = 0
     for n in roman_string:
