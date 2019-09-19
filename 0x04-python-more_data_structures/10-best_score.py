3e5416475ca5853f006c76bece9f7aaef47e47a3
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-19 19:54:18 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return
-
-    my_list = list(a_dictionary.keys())
-    key = my_list[0]
-#    print("key = {}".format(key))
-
-    max = int(a_dictionary[key])
-    for i in my_list:
+        return None
+    key = ""
+    max = 0
+    for i in a_dictionary:
         if int(a_dictionary[i]) > max:
             max = int(a_dictionary[i])
             key = i
-#            print("Este = {}".format(key))
     return key
