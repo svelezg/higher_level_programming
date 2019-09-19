42b66e13921a6832894c1ce195ad80f11c81b853
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-19 18:59:56 +0000

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] = a_dictionary[key] * 2
-    return a_dictionary
+    return {x: a_dictionary[x]*2 for x in a_dictionary}
