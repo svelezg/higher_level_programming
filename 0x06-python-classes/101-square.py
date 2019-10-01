0510fb91d5b80595ac84877587d22f148bc4cf1a
svelezg <47928168+svelezg@users.noreply.github.com>
2019-10-01 22:12:02 +0000

    Task 101

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """constructor"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        if (not isinstance(position, tuple) or len(position) != 2 or
+            not isinstance(position[0], int) or
+                not isinstance(position[1], int) or
+                position[0] < 0 or position[1] < 0):
+                raise TypeError("position must be a " +
+                                "tuple of 2 positive integers")
+        self.__position = position
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """getter for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter for position"""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+            not isinstance(value[0], int) or
+                not isinstance(value[1], int) or
+                value[0] < 0 or value[1] < 0):
+                raise TypeError("position must be a " +
+                                "tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """area method definition"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """my_print method definition"""
+        if self.__size <= 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                    print()
+            for i in range(self.__size):
+                for i in range(self.__position[0]):
+                    print(" ", end="")
+                for i in range(self.__size):
+                    print("#", end="")
+                print()
+
+    def __str__(self):
+        """ """
+        if self.__size <= 0:
+            return ""
+        else:
+            return (self.__position[1] * "\n" +
+                    self.__size * (self.__position[0] * " " +
+                    self.__size * "#" + "\n"))
