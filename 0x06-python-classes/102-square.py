5d0244302115890a25bcaf2d1e485e0835a544c3
svelezg <47928168+svelezg@users.noreply.github.com>
2019-10-02 01:23:27 +0000

    Task 102

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -10,10 +10,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
-        """returns the current square area"""
-        return self.__size ** 2
-
     @property
     def size(self):
         """property to retrieve it"""
@@ -22,38 +18,30 @@ class Square:
     @size.setter
     def size(self, value):
         """property setter to set it """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+        if (not isinstance(value, int) and not isinstance(value, float)):
+            raise TypeError("size must be an number")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def __lt__(self, other):
-        "compares areas"
-        if((self.__size ** 2) < (other.__size ** 2)):
-            return True
+        return (self.size < other.size)
 
     def __gt__(self, other):
-        "compares areas"
-        if(self.area() > other.area()):
-            return True
+        return (self.area() > other.area())
 
     def __le__(self, other):
-        "compares areas"
-        if(self.area() <= other.area()):
-            return True
+        return (self.area() <= other.area())
 
     def __ge__(self, other):
-        "compares areas"
-        if(self.area() >= other.area()):
-            return True
+        return (self.area() >= other.area())
 
     def __eq__(self, other):
-        "compares areas"
-        if(self.area() == other.area()):
-            return True
+        return (self.size == other.size)
 
     def __eq__(self, other):
-        "compares areas"
-        if(self.area() != other.area()):
-            return True
+        return (self.area() == other.area())
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
