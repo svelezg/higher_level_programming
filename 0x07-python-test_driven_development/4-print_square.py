64a531c45e71e8bd175dcbf0a18bc49f8ddc950b
svelezg <47928168+svelezg@users.noreply.github.com>
2019-10-06 19:18:30 +0000

    size zero

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,8 +5,6 @@ This is the "4-print_square" module.
 The 4-print_square module supplies one function, print_square(size).
 """
 
-import doctest
-
 
 def print_square(size):
     """print_square function
@@ -20,11 +18,11 @@ def print_square(size):
         raise ValueError("size must be >= 0")
 
     str = ""
-
-    for i in range(size):
-        for j in range(size):
-            str += '#'
-        str += '\n'
-    print(str[:-1])
-
-# doctest.testfile(4-print_square, verbose=False)
+    if size == 0:
+        print()
+    else:
+        for i in range(size):
+            for j in range(size):
+                str += '#'
+            str += '\n'
+        print(str[:-1])
