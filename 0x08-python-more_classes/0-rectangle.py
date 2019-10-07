c6bbf5c57c58b987501d4bd8ed6f910f4582d155
svelezg <47928168+svelezg@users.noreply.github.com>
2019-10-07 15:58:35 +0000

    Task 0 to 9

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Rectangle:
+    """empty Rectangle class"""
+    pass  # An empty block
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+class Rectangle:
+    """empty Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """constructor"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("heigth must be an integer")
+        if value < 0:
+            raise ValueError("heigth must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """area method definition"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """perimeter method definition"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
