ad4d9808aab07cc74ecd216c849a561406640db8
svelezg <47928168+svelezg@users.noreply.github.com>
2019-10-08 23:46:00 +0000

    Task 100 and 101

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-class magic_string:
-    """magic_string class"""
-
-    number_of_instances = 0
-
-    def __init__(self):
-        """constructor"""
-        magic_string.number_of_instances += 1
-
-    def __str__(self):
-        """end user output"""
-        string = ""
-        for i in range(0, magic_string.number_of_instances - 1):
-            string = string + "Holberton, "
-        string = string + "Holberton"
-        return string
+def magic_string(str=[]):
+    str.append("Holberton")
+    return ", ".join(str)
