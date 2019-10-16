8d0dacb34e177002e20759cb8af45504c0cd5bd9
svelezg <47928168+svelezg@users.noreply.github.com>
2019-10-16 23:29:19 +0000

    Task 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -8,13 +8,15 @@ It supplies one function, pascal_triangle.
 def pascal_triangle(n):
     """pascal_triangle"""
 
-    triangle =[[1]]
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
     for i in range(1, n):
         row = [1]
-        for j in range(1,i):
+        for j in range(1, i):
             elem = triangle[i-1][j-1] + triangle[i-1][j]
             row.append(elem)
         row.append(1)
-        triangle.append(row)   
-        
+        triangle.append(row)
     return triangle
