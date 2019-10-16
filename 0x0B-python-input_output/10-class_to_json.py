0ebe0a0f911a226783c83b31642bfa0f5d4969a9
svelezg <47928168+svelezg@users.noreply.github.com>
2019-10-16 23:14:38 +0000

    Task 10

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -4,10 +4,11 @@ This is the "10-class_to_json" module.
 It supplies one class, class_to_json.
 """
 
+
 import json
+
 
 def class_to_json(obj):
     """class_to_json function"""
 
-    if obj.__dict__:
-        return obj.__dict__
+    return obj.__dict__
