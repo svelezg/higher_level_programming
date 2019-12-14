bb403a18b0037c2236951d3fb0b3af3f20fe5a98
svelezg <svelez.velezgarcia@gmail.com>
2019-12-14 11:59:56 -0500

    Task 9 to 14

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import MultipleResultsFound
+from sqlalchemy import insert
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # create session
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    # query = session.query(State).filter(State.name == argv[4]).order_by(State.id).first()
+
+    my_input = 'Louisiana'
+
+    new_state = State(name=my_input)
+    session.add(new_state)
+    session.flush()
+    session.commit()
+
+    query = session.query(State).filter(State.name == my_input).order_by(State.id).first()
+
+    if query is None:
+        print('Not found')
+    else:
+        print(query.id)
---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # create session
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    selection = select('*').select_from(City).order_by(City.id).group_by(State.id)
+    result = session.execute(selection).fetchall()
+    for _row in result:
+        print('{}: {}'.format(_row.id, _row.name))
+
+
