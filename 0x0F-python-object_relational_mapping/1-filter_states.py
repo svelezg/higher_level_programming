32791635fa104918110d6ae9f5ed66a949592993
svelezg <svelez.velezgarcia@gmail.com>
2019-12-14 13:35:47 -0500

    Task 1 and 2 correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,16 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states "
+                "WHERE name LIKE 'N%' "
+                "ORDER BY id ASC")
 
     rows = cur.fetchall()
     for row in rows:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,18 +7,19 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
 
     name = argv[4]
     cur.execute("""SELECT *
-    FROM states WHERE name = '{}'
+    FROM states
+    WHERE name = '{}'
     ORDER BY id""".format(argv[4]))
 
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    cur.close()
-    db.close()
