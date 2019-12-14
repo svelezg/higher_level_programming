5deaf184c0b15632f525250cbbd7dd747acf21cf
svelezg <svelez.velezgarcia@gmail.com>
2019-12-14 13:44:23 -0500

    Task 1 and 2 correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,18 +4,22 @@ lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
 """
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     import MySQLdb
+
+    my_user = sys.argv[1]
+    my_pass = sys.argv[2]
+    my_db = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+                         user=my_user,
+                         passwd=my_pass,
+                         db=my_db)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states "
-                "WHERE name LIKE 'N%' "
+                "WHERE name LIKE BINARY 'N%' "
                 "ORDER BY id ASC")
 
     rows = cur.fetchall()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,21 +4,25 @@ takes in an argument and displays all values in
 the states table of hbtn_0e_0_usa where name matches the argument.
 """
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     import MySQLdb
+
+    my_user = sys.argv[1]
+    my_pass = sys.argv[2]
+    my_db = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+                         user=my_user,
+                         passwd=my_pass,
+                         db=my_db)
     cur = db.cursor()
 
-    name = argv[4]
+    name = sys.argv[4]
     cur.execute("""SELECT *
     FROM states
     WHERE name = '{}'
-    ORDER BY id""".format(argv[4]))
+    ORDER BY id""".format(name))
 
     rows = cur.fetchall()
     for row in rows:
