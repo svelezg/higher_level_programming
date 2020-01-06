04a81df5f868d99ee2a136919581c9a81b0b089a
svelezg <svelez.velezgarcia@gmail.com>
2020-01-06 15:38:13 -0500

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-curl $1
+# takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -Lsf "$1"
