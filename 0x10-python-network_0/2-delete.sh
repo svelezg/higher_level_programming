21da27bc1ddd9e20ca0266269407460e5e196012
svelezg <svelez.velezgarcia@gmail.com>
2020-01-06 15:50:18 -0500

    Task 2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# ttakes in a URL, sends a GET request to the URL, and displays the body of the response
-
-curl $1
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -sX "DELETE" "$1"
