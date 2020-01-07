68b1e58c12dfc84361e9a28cfdc1ab95319bf124
svelezg <svelez.velezgarcia@gmail.com>
2020-01-06 21:09:39 -0500

    Right file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d @"$2" "$1"
+# Catch me if you can!
+curl -sLX PUT -d 'user_id=98' -H 'Origin: HolbertonSchool' 0.0.0.0:5000/catch_me
