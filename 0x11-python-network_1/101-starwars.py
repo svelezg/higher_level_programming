259f5d57261ab1d8c6789b4f5bd672fbc1c3472f
svelezg <svelez.velezgarcia@gmail.com>
2020-01-13 16:26:02 -0500

    Task 101

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+takes in a string and sends a search request to the Star Wars API
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = 'https://swapi.co/api/people'
+
+    if len(sys.argv) != 2:
+        search = ""
+        print("No result")
+    else:
+        search = sys.argv[1]
+        values = {'search': search}
+        response = requests.get(url, params=values)
+        print("Number of results: {}".format(response.json().get("count")))
+        results = response.json().get("results")
+        for result in results:
+            print(result.get("name"))
+
+        while 1:
+            url = response.json().get("next")
+            response = requests.get(url, params=values)
+            results = response.json().get("results")
+            for result in results:
+                print(result.get("name"))
+
+            if response.json().get("next") is None:
+                break
