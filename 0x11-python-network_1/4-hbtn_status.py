8073374e842f25f77e8a229aaf7ef00952166ed4
svelezg <svelez.velezgarcia@gmail.com>
2020-01-13 12:59:51 -0500

    Task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+script that fetches https://intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import requests
+
+    response = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.content.decode('utf-8')))
