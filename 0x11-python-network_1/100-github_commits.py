c2e9d97cbca495559e027a8d0b361883262fb744
svelezg <svelez.velezgarcia@gmail.com>
2020-01-13 15:21:26 -0500

    Task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = "https://api.github.com/repos/{}/{}/commits".\
+        format(sys.argv[1], sys.argv[2])
+    param = {"per_page": "10"}
+    response = requests.get(url, params=param)
+
+    for result in response.json():
+        print("{}: {}".format(result['sha'],
+                              result['commit']['author']['name']))
