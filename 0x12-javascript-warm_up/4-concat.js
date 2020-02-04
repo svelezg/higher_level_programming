350366526dff7981921e70583bd282da519e8bf1
svelezg <svelez.velezgarcia@gmail.com>
2020-02-04 13:04:32 -0500

    Task 4 correction

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const myVar = ['No arguments', 'Argument found', 'Arguments found'];
-if (!process.argv[3]) {
-  console.log(myVar[0]);
-} else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
