f6b1630644d59b52f1d7ada58f855f6567cde99e
svelezg <svelez.velezgarcia@gmail.com>
2020-02-11 12:44:59 -0500

    Task 0 correction

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) throw err;
-
-  console.log(data);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
