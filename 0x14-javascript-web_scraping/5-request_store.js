8b88d76c931b2622af4973b0f0a11bfeabd5aae4
svelezg <svelez.velezgarcia@gmail.com>
2020-02-11 13:05:34 -0500

    Task 5 correction

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,15 +3,13 @@
 const fs = require('fs');
 const request = require('request');
 const url = process.argv[2];
+const fileName = process.argv[3];
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    // Data which will write in a file.
-    const data = body;
-    // Write data in 'process.argv[2]' .
-    fs.writeFile('loripsum', data, 'utf-8', (err) => {
+    fs.writeFile(fileName, body, 'utf-8', (err) => {
       // In case of a error throw err.
       if (err) throw err;
     });
